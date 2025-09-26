036cd8fc80c68ede8e08291692ec39d906bfd2fe
Alessandro Chaves <arroschaves@gmail.com>
2025-09-26 15:45:40 -0400

    Renomeando arquivo temporário para corrigir case-sensitive

---
+++ app/sections/QuemSomosTemp.tsx
@@ -0,0 +1,11 @@
+export default function QuemSomos() {
+  return (
+    <section className="p-10 bg-green-800 text-white text-center">
+      <h3 className="text-3xl font-bold text-yellow-400 mb-4">Quem Somos</h3>
+      <p>
+        Escritório Brandão Contabilidade, há anos prestando serviços contábeis de excelência
+        em Sidrolândia - MS.
+      </p>
+    </section>
+  );
+}
