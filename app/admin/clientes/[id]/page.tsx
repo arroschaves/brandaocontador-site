5fd4df070369297d5433f2dbfa6c47bc0f3e92a1
arroschaves <professormatms@bol.com.br>
2026-02-06 14:11:08 -0400

    fix: mover PendenciaModal para dentro do return JSX

--- app/admin/clientes/[id]/page.tsx
+++ app/admin/clientes/[id]/page.tsx
@@ -1070,7 +1070,7 @@ export default function ClientHubPage({ params }: { params: Promise<{ id: string
     )
 }
 
-{/* Modal de Mapeamento Manual Maestro */}
+{/* Modal de Mapeamento Manual Maestro */ }
 function MappingModal({ showMappingModal, setShowMappingModal, mappingLoading, mappingData, client, selectedRoutine, handleManualLink }: any) {
     if (!showMappingModal) return null;
     return (
@@ -1138,7 +1138,7 @@ function MappingModal({ showMappingModal, setShowMappingModal, mappingLoading, m
             </div>
         </div>
         
-        {/* Modal de Pendência */}
+        {/* Modal de Pendência */ }
     <PendenciaModal
         isOpen={showPendenciaModal}
         onClose={() => {
@@ -1149,5 +1149,6 @@ function MappingModal({ showMappingModal, setShowMappingModal, mappingLoading, m
         agendamento={agendamentoEditando}
         clientId={clientId}
     />
+    </div >
     );
 }
