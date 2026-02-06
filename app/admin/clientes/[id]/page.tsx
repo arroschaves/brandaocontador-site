fac4320372e6e78ee57931e678ba86c720d61e0b
arroschaves <professormatms@bol.com.br>
2026-02-06 14:17:34 -0400

    revert: voltar page.tsx para versão funcional (7b907bb)

--- app/admin/clientes/[id]/page.tsx
+++ app/admin/clientes/[id]/page.tsx
@@ -1137,6 +1137,17 @@ function MappingModal({ showMappingModal, setShowMappingModal, mappingLoading, m
                 </div>
             </div>
         </div>
-    </div >
+        
+        {/* Modal de Pendência */ }
+    <PendenciaModal
+        isOpen={showPendenciaModal}
+        onClose={() => {
+            setShowPendenciaModal(false)
+            setAgendamentoEditando(null)
+        }}
+        onSave={handleSalvarAgendamento}
+        agendamento={agendamentoEditando}
+        clientId={clientId}
+    />
     );
 }
